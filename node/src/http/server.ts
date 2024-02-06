7eaa88a095e5cadaf3321590de6da505a30d5f02
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2024-02-06 17:23:09 -0300

    chore: initial commit

--- node/src/http/server.ts
+++ node/src/http/server.ts
@@ -1,11 +1,19 @@
 import { fastify } from "fastify";
-
+import { z } from "zod";
 const app = fastify();
 
-app.get('/', async () => {
-  return "Hello World"
+app.post("/polls", async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPollBody.parse(request.body);
+
+  return reply.status(201).send({
+    title
+  })
 })
 
 app.listen({ port: 3000 }).then(() => {
-  console.log('HTTP server running on http://localhost:3000')
+  console.log("[SERVER] HTTP server listening on port 3000");
 })
